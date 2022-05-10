e1315dafcca2651ece75751b167cfd1bd0468c76
Wil Freitas <91095076+wilson-freitas@users.noreply.github.com>
2022-05-10 15:13:48 -0300

    Update run.py

--- OOP/run.py
+++ OOP/run.py
@@ -6,6 +6,6 @@ class running(limoRacing):
     def runGame(self, game_engine):
         return super().runGame(game_engine)
     
-carro = running(game_engine, flag_soma, background, pitu, carro_azul, carro_verde, texto, fonte, fonte_padrao, relogio, tela, x_verde, y_verde, x_azul, y_azul, x_pitu, y_pitu)
+carro = running(game_engine, flag_soma, background, pitu, carro_azul, carro_verde, texto, fonte, fonte_padrao, relogio, tela, x_verde, y_verde, x_azul, y_azul, x_pitu, y_pitu, bitcoin, x_bitcoin, y_bitcoin)
 
 carro.runGame(game_engine)
